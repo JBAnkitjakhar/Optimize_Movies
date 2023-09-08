e82530f7ca56372543c2e5f5ce062884c95927fb
Amit <amitjakhar@iitbhilai.ac.in>
2023-09-08 13:13:46 +0530

    updated2

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,3 +1,67 @@
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+// import "./style.scss";
+
+// import useFetch from "../../../hooks/useFetch";
+
+// import Img from "../../../components/lazyLoadImage/Img";
+// import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+// const HeroBanner = () => {
+//     const [background, setBackground] = useState("");
+//     const [query, setQuery] = useState("");
+//     const navigate = useNavigate();
+//     const { url } = useSelector((state) => state.home);
+//     const { data, loading } = useFetch("/movie/upcoming");
+
+//     useEffect(() => {
+//         const bg =
+//             url.backdrop +
+//             data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+//         setBackground(bg);
+//     }, [data]);
+
+//     const searchQueryHandler = (event) => {
+//         if (event.key === "Enter" && query.length > 0) {
+//             navigate(`/search/${query}`);
+//         }
+//     };
+
+
+//     return (
+//         <div className="heroBanner">
+//             {!loading && (
+//                 <div className="backdrop-img">
+//                     <Img src={background} />
+//                 </div>
+//             )}
+
+//             <div className="opacity-layer"></div>
+//             <ContentWrapper>
+//                 <div className="heroBannerContent">
+//                     <span className="title">Welcome.</span>
+//                     <span className="subTitle">
+//                        Explore your Movies and watch trailers also find what's coming next!!!
+//                     </span>
+//                     <div className="searchInput">
+//                         <input
+//                             type="text"
+//                             placeholder="Search for a movie or tv show...."
+//                             onChange={(e) => setQuery(e.target.value)}
+//                             onKeyUp={searchQueryHandler}
+//                         />
+//                         <button>Search</button>
+//                     </div>
+//                 </div>
+//             </ContentWrapper>
+//         </div>
+//     );
+// };
+
+// export default HeroBanner;
+
+
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -22,10 +86,14 @@ const HeroBanner = () => {
         setBackground(bg);
     }, [data]);
 
-    const searchQueryHandler = (event) => {
-        if (event.key === "Enter" && query.length > 0) {
+    const searchQueryHandler = () => {
+        if (query.length > 0) {
             navigate(`/search/${query}`);
         }
+    };
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
     };
 
     return (
@@ -41,17 +109,26 @@ const HeroBanner = () => {
                 <div className="heroBannerContent">
                     <span className="title">Welcome.</span>
                     <span className="subTitle">
-                        Millions of movies, TV shows and people to discover.
-                        Explore now.
+                        Explore your Movies and watch Trailers also find what's coming <span className="next-word">NEXT</span>
                     </span>
                     <div className="searchInput">
                         <input
                             type="text"
                             placeholder="Search for a movie or tv show...."
-                            onChange={(e) => setQuery(e.target.value)}
-                            onKeyUp={searchQueryHandler}
+                            onChange={handleInputChange}
+                            value={query}
+                            onKeyUp={(e) => {
+                                if (e.key === "Enter") {
+                                    searchQueryHandler();
+                                }
+                            }}
                         />
-                        <button>Search</button>
+                        <button
+                            onClick={searchQueryHandler}
+                            disabled={query.length === 0}
+                        >
+                            Search
+                        </button>
                     </div>
                 </div>
             </ContentWrapper>
